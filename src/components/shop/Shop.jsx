a83e66ede3d7e5dc97915762b87ff3c3e6ac5722
MetalSnow <aymanetrend@gmail.com>
2024-11-14 01:17:53 +0800

    feat(shop): Add category filtering with unique category buttons and active styling

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -4,14 +4,43 @@ import NavBar from '../nav/NavBar';
 import CardItem from './CardItem';
 import styles from './Shop.module.css';
 
+const url = 'https://fakestoreapi.com/products';
+
 function Shop() {
-  const [products, setProducts] = useState([]);
+  const [initialProducts, setInitialProducts] = useState([]);
+  const [products, setProducts] = useState(null);
+  const [activeCategory, setActiveCategory] = useState('');
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch(url, { mode: 'cors' })
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        console.log(data);
+        setInitialProducts(data);
+        setProducts(data);
+      });
   }, []);
+
+  const handleCategory = (event) => {
+    const categorySelected = event.target.textContent;
+    setActiveCategory(categorySelected);
+
+    if (categorySelected === 'Show all products') {
+      setProducts(initialProducts);
+      return;
+    }
+
+    const filtredProducts = initialProducts.filter((product) => {
+      console.log(product.category);
+      return product.category === categorySelected.toLowerCase() && product;
+    });
+
+    setProducts(filtredProducts);
+  };
+
+  const uniqueCategories = [
+    ...new Set(initialProducts.map((product) => product.category)),
+  ];
 
   return (
     <>
@@ -21,24 +50,48 @@ function Shop() {
         <aside className={styles.aside}>
           <p>Category</p>
           <ul>
-            <li>coats</li>
-            <li>shirts</li>
-            <li>pants</li>
-            <li>shoes</li>
+            {uniqueCategories.map((category, index) => {
+              const categoryName =
+                category.charAt(0).toUpperCase() + category.slice(1);
+
+              return (
+                <li key={index}>
+                  <button
+                    onClick={handleCategory}
+                    className={
+                      activeCategory === categoryName
+                        ? styles.activeCategory
+                        : ''
+                    }
+                  >
+                    {categoryName}
+                  </button>
+                </li>
+              );
+            })}
+            <li>
+              <button onClick={handleCategory} className={styles.allProducts}>
+                Show all products
+              </button>
+            </li>
           </ul>
         </aside>
         <ul className={styles.listCards}>
-          {products.map((product) => (
-            <li key={product.id}>
-              <CardItem
-                title={product.title}
-                description={product.description}
-                image={product.image}
-                price={product.price}
-                rating={product.rating}
-              />
-            </li>
-          ))}
+          {products ? (
+            products.map((product) => (
+              <li key={product.id}>
+                <CardItem
+                  title={product.title}
+                  description={product.description}
+                  image={product.image}
+                  price={product.price}
+                  rating={product.rating}
+                />
+              </li>
+            ))
+          ) : (
+            <p style={{ textAlign: 'center', fontSize: 18 }}>Loading...</p>
+          )}
         </ul>
       </div>
     </>
